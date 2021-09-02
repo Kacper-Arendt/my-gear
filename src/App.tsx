43c13d07e0a7e242b01525fc96c878faff1acc21
kacper-arendt <kacykvaa@gmail.com>
2021-09-02 19:21:54 +0200

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {UserLogin} from "./components/Components";
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
+    letter-spacing: 1.4px;
+  }
+`
 
 function App() {
-  return (
-    <div>
-     <h1>hello</h1>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyles/>
+            <Router>
+                <Switch>
+                    <Route path='/login'>
+                        <UserLogin/>
+                    </Route>
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Auth/UserLogin.tsx
@@ -0,0 +1,9 @@
+
+
+export const UserLogin = () => {
+    return (
+        <>
+            <h1>hello</h1>
+        </>
+    )
+}
