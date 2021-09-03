310403966ac3a125ec4d112ef0cd5f1d88162515
kacper-arendt <kacykvaa@gmail.com>
2021-09-03 09:12:11 +0200

    feat add reusable input

--- src/components/Auth/UserLogin.tsx
+++ src/components/Auth/UserLogin.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import {Input} from '../UI/UIComponents';
 
 export const UserLogin = () => {
     return (
         <>
             <form>
-                <input type="email'"/>
-                <input type="password"/>
+                <Input
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                />
+                <Input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                />
                 <button>Login</button>
                 <a href="/register">Create an account</a>
             </form>
---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+interface InputProps {
+    type: string,
+    id?: string,
+    name: string,
+    value?: string,
+    placeholder: string,
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+const StyledInput = styled.input`
+  width: 90%;
+  max-width: 25rem;
+  margin: .8rem;
+  padding: 0.7rem;
+  border: .15rem solid black;
+  font-size: inherit;
+  font-weight: 700;
+  letter-spacing: 1px;
+  cursor: pointer;
+  outline: none;
+  transition: all 0.4s;
+
+  ::placeholder {
+    color: rgba(0, 0, 0, .7);
+  }
+
+  :hover, :focus {
+    transform: scale(1.02);
+  }
+
+  :focus {
+    transform: scale(1.05);
+    border: .2rem solid orange;
+  }
+`
+
+export const Input = (props: InputProps) => {
+    return (
+        <StyledInput
+            type={props.type}
+            id={props.id}
+            name={props.name}
+            value={props.value}
+            onBlur={props.onChange}
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+        />
+    )
+}
